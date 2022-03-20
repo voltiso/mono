32295c7f4c2cb5cb13740f53d32f716de9dec9e1
Adam Blaszkiewicz <adam.blaszkiewicz@voltiso.com>
2022-03-20 01:43:27 +0100

    ...

--- src/object/merge/merge.ts
+++ src/object/merge/merge.ts
@@ -1,15 +1,18 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import { Flatten } from '../flatten'
 
-type MergeI<acc, objs> = objs extends readonly []
+type _MergeN<objs, acc> = objs extends readonly []
 	? acc
 	: objs extends readonly [infer h, ...infer t]
 	? [h] extends [object]
-		? MergeI<Flatten<Omit<acc, keyof h> & h>, t>
+		? _MergeN<t, Flatten<Omit<acc, keyof h> & h>>
 		: [h] extends [never]
-		? MergeI<acc, t>
+		? _MergeN<t, acc>
 		: acc
 	: never
+type MergeN<objs> = _MergeN<objs, {}>
+
+export type Merge2<A, B> = Flatten<Omit<A, keyof B> & B>
 
 export type Merge<
 	A extends readonly object[] | object,
@@ -17,12 +20,12 @@ export type Merge<
 	C extends [A] extends [readonly object[]] ? void : object | void = void,
 	D extends [A] extends [readonly object[]] ? void : object | void = void,
 	E extends [A] extends [readonly object[]] ? void : object | void = void
-> = [A] extends [readonly object[]] ? MergeI<{}, A> : MergeI<{}, readonly [A, B, C, D, E]>
+> = [A] extends [readonly object[]] ? MergeN<A> : MergeN<readonly [A, B, C, D, E]>
 
-export function merge<Objs extends readonly object[]>(...objs: Objs): Merge<Objs> {
+export function merge<Objs extends readonly object[]>(...objs: Objs): MergeN<Objs> {
 	let r = {}
 	for (const obj of objs) {
 		r = { ...r, ...obj }
 	}
-	return r as Merge<Objs>
+	return r as MergeN<Objs>
 }
