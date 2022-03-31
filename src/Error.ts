fc935b67950c23d294f89c3e5c2cd0693b2a3850
Adam Blaszkiewicz <adam.blaszkiewicz@voltiso.com>
2022-04-01 01:22:04 +0200

    renamed Error to Throw

--- src/Error.ts
+++ src/Error.ts
@@ -1,1 +1,6 @@
-export type Error<message> = message
+// eslint-disable-next-line @typescript-eslint/ban-types
+type TypeError = {
+	Error: TypeError
+}
+
+export type Throw<message> = TypeError & message
