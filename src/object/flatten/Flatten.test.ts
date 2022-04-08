8eec380361addb667ef36316966f394f25641aea
Adam Blaszkiewicz <adam.blaszkiewicz@voltiso.com>
2022-04-09 00:58:19 +0200

    Flatten updates (?)

--- src/object/flatten/Flatten.test.ts
+++ src/object/flatten/Flatten.test.ts
@@ -1,14 +1,19 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import { Assert } from '../../assert'
-import { IsIdentical } from '../../IsEqual'
+import { IsCompatible, IsIdentical } from '../../IsEqual'
 import { Flatten, Flatten2 } from './Flatten'
 
 describe('Flatten', () => {
 	it('works', () => {
 		expect.assertions(0)
 
-		Assert<IsIdentical<Flatten<{ a: 1 } & { b: 2 }>, { a: 1; b: 2 }>>()
+		type A = Flatten<{ a: 1 } & { b?: 2 }>
+		Assert<IsIdentical<A, { a: 1; b?: 2 }>>()
 		Assert<IsIdentical<Flatten<{ a?: 1 }>, { a?: 1 }>>()
 		Assert<IsIdentical<Flatten<{ a?: 1 | undefined }>, { a?: 1 }>, false>()
+		Assert<IsIdentical<Flatten<{ a: 1 | undefined }>, { a?: 1 }>, false>()
+		Assert<IsIdentical<Flatten<{ a: 1 | undefined }>, { a?: 1 | undefined }>, false>()
+		Assert<IsIdentical<Flatten<{ a: 1 | undefined }>, { a: 1 | undefined }>>()
 
 		Assert<Flatten<number>, number>()
 		Assert<number, Flatten<number>>()
@@ -19,8 +24,9 @@ describe('Flatten', () => {
 		Assert<Date, Flatten<Date>>()
 		Assert<Flatten<Date>, Date>()
 
-		Assert<typeof Date, Flatten<typeof Date>>()
-		Assert<Flatten<typeof Date>, typeof Date>()
+		type TypeofDate = typeof Date
+		type FTypeofDate = Flatten<TypeofDate>
+		Assert<IsCompatible<FTypeofDate, TypeofDate>>()
 
 		type Rec = Rec[] | string
 		Assert<Rec, Flatten<Rec>>()
@@ -49,6 +55,24 @@ describe('Flatten', () => {
 			IsIdentical<
 				Flatten2<
 					{
+						a: { a?: 1 }
+					} & {
+						a?: { b: 2 }
+					}
+				>,
+				{
+					a: {
+						a?: 1
+						b: 2
+					}
+				}
+			>
+		>()
+
+		Assert<
+			IsIdentical<
+				Flatten<
+					{
 						a: { a: 1 }
 					} & {
 						a: { b: 2 }
@@ -60,7 +84,84 @@ describe('Flatten', () => {
 						b: 2
 					}
 				}
-			>
+			>,
+			false
 		>()
+	})
+
+	it('primitives', () => {
+		expect.assertions(0)
+		type N = Flatten<number>
+		Assert<IsIdentical<N, number>>()
+
+		type S = Flatten<string>
+		Assert<IsIdentical<S, string>>()
+
+		type O = Flatten<object>
+		Assert<IsIdentical<O, object>>()
+
+		type OO = Flatten<{}>
+		Assert<IsIdentical<OO, {}>>()
+
+		type OOO = Flatten<Object>
+		Assert<IsIdentical<OOO, Object>>()
+
+		type OOOO = Flatten<Record<string, unknown>>
+		Assert<IsIdentical<OOOO, Record<string, unknown>>>()
+
+		type B = Flatten<boolean>
+		Assert<IsIdentical<B, boolean>>()
+
+		type U = Flatten<unknown>
+		Assert<IsIdentical<U, unknown>>()
+
+		type Null = Flatten<null>
+		Assert<IsIdentical<Null, null>>()
+
+		type Undef = Flatten<undefined>
+		Assert<IsIdentical<Undef, undefined>>()
+	})
+
+	it('works with index signatures', () => {
+		expect.assertions(0)
+		type X = Flatten<{ b: 2; [k: string]: 1 | 2 } & { a: 1 }>
+		Assert<IsIdentical<X, { [k: string]: 1 | 2; a: 1; b: 2 }>>()
+	})
+
+	// eslint-disable-next-line jest/no-commented-out-tests
+	// it('works with call signatures', () => {
+	// 	expect.assertions(0)
+	// 	type X = Flatten<((str: string) => number) & { a: 1 } & {b: 2}>
+	// })
+
+	it('works with construct signatures', () => {
+		expect.assertions(0)
+
+		type X = Flatten<new (n: number) => string>
+		Assert<IsIdentical<X, new (n: number) => string>>()
+
+		type XX = Flatten<(new (x: 22) => string) & { a?: 1 }>
+		Assert<IsCompatible<XX, { new (x: 22): string; a?: 1 }>>()
+		// Assert<IsIdentical<XX, { new (x: 22): string; a: 1 }>>()
+	})
+
+	// eslint-disable-next-line jest/no-commented-out-tests
+	// it('works with call and construct signatures', () => {
+	// 	expect.assertions(0)
+	// 	type X = Flatten<((str: string) => number) & ((x: 22) => string) & { a: 1 }>
+	// })
+
+	it('works with generics (unknown)', <X>() => {
+		expect.assertions(0)
+		type Y = Flatten<X>
+		Assert<Y, X>()
+		// Assert<X, Y>()
+	})
+
+	it('works with generics (string)', <X extends string>() => {
+		expect.assertions(0)
+		type Y = Flatten<X>
+		Assert<Y, X>()
+		// Assert<X, Y>()
 	})
 })
--- src/object/flatten/Flatten.ts
+++ src/object/flatten/Flatten.ts
@@ -1,15 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Callable, Newable } from '../../types'
-import { Field } from '../entries/Field'
 
-export type Flatten<T> = T extends Callable<any[]>
-	? T
-	: T extends Newable<any[]>
-	? T
-	: [{ [k in keyof T]: Field<T, k> }][0]
+export type FlattenObject<O extends object> = { [k in keyof O]: O[k] }
 
-export type Flatten2<T> = T extends Callable<any[]>
+export type Flatten<T> = T extends abstract new (...args: never[]) => unknown
 	? T
-	: T extends Newable<any[]>
+	: T extends (...args: never[]) => unknown
 	? T
-	: [{ [k in keyof T]: Flatten<Field<T, k>> }][0]
+	: T extends object
+	? FlattenObject<T>
+	: T
+
+export type Flatten2<T> = T extends abstract new (...args: never[]) => unknown
+	? T
+	: T extends (...args: never[]) => unknown
+	? T
+	: T extends object
+	? { [k in keyof T]: Flatten<T[k]> }
+	: T
