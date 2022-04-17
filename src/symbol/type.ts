56e5dd55d8f527d0a5d76c4c2b98fc519ff9e05d
Adam Blaszkiewicz <adam.blaszkiewicz@voltiso.com>
2022-04-18 00:26:02 +0200

    try to get rid of unique symbol warning in client code - esbuild

--- src/symbol/type.ts
+++ src/symbol/type.ts
@@ -1,2 +1,6 @@
-export declare const type: unique symbol
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+// export declare const type: unique symbol
+export const type: unique symbol = 0 as any // get rid of esbuild warning
 export type type = typeof type
