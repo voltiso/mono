3e50cc685e87b08fca30f4661fd0cbfbcbd11d10
Adam Blaszkiewicz <adam.blaszkiewicz@voltiso.com>
2022-04-25 02:54:17 +0200

    added object/Nest

--- src/object/index.ts
+++ src/object/index.ts
@@ -10,6 +10,7 @@ export * from './isPlain'
 export * from './omit'
 export * from './pick'
 export * from './isConstructorOrProto'
+export * from './Nest'
 
 export type { Flatten as _ } from './flatten'
 export type { Flatten2 as __ } from './flatten'
