e7c6da4659d8fedc4c5b908c729bfa541776b4fa
Adam Blaszkiewicz <adam.blaszkiewicz@voltiso.com>
2022-05-20 02:11:13 +0200

    added HasSignatures

--- src/object/omit/OmitSignatures.test.ts
+++ src/object/omit/OmitSignatures.test.ts
@@ -1,6 +1,7 @@
+/* eslint-disable no-magic-numbers */
 import { Assert } from '../../bdd'
 import { IsIdentical } from '../../IsEqual'
-import { OmitSignatures } from './OmitSignatures'
+import { HasSignatures, OmitSignatures } from './OmitSignatures'
 
 describe('OmitSignatures', () => {
 	it('works', () => {
@@ -12,5 +13,15 @@ describe('OmitSignatures', () => {
 			num: number
 		}>
 		Assert<IsIdentical<X, { num: number }>>()
+	})
+
+	it('HasSignatures', () => {
+		expect.assertions(0)
+
+		Assert<IsIdentical<HasSignatures<{ a: 0 }>, false>>()
+		Assert<IsIdentical<HasSignatures<{ (): void; a: 0 }>, true>>()
+		Assert<IsIdentical<HasSignatures<{ [k: string]: unknown; a: 0 }>, true>>()
+		Assert<IsIdentical<HasSignatures<{ a: 1 } & { b: 2 }>, false>>()
+		Assert<IsIdentical<HasSignatures<{ a: 1 } & { b: 2; [k: number]: number }>, true>>()
 	})
 })
--- src/object/omit/OmitSignatures.ts
+++ src/object/omit/OmitSignatures.ts
@@ -1,7 +1,15 @@
 // https://stackoverflow.com/a/51956054/1123898
+
+import { IsEqual } from '../../IsEqual'
+
 /**
  * Omit call, construct and index signatures
  */
 export type OmitSignatures<T> = {
 	[K in keyof T as string extends K ? never : number extends K ? never : K]: T[K]
 }
+
+/**
+ * Has call, construct or index signature
+ */
+export type HasSignatures<X extends object, T = true, F = false> = IsEqual<X, OmitSignatures<X>, F, T>
