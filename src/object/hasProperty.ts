ae62799d874aac1bc3abb87e4412773edab93f69
Adam Blaszkiewicz <adam.blaszkiewicz@voltiso.com>
2022-05-21 15:13:03 +0200

    hasProperty: returns false instead of throwing when typeof o !== 'object'

--- src/object/hasProperty.ts
+++ src/object/hasProperty.ts
@@ -1,9 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { assert } from '../assert'
 import { checkPrototypePollution } from './isConstructorOrProto'
 
 export function hasProperty<O extends object, K extends keyof any>(o: O, k: K): k is K & keyof O {
-	assert(typeof o === 'object')
+	if (typeof o !== 'object') return false
 	checkPrototypePollution(o, k)
 	return k in o
 }
