689f12196440a41d8e584fc049223e1a0079ff4a
Adam Blaszkiewicz <adam.blaszkiewicz@voltiso.com>
2022-05-21 15:06:43 +0200

    object: hasProperty: improved typings

--- src/object/hasProperty.ts
+++ src/object/hasProperty.ts
@@ -2,7 +2,7 @@
 import { assert } from '../assert'
 import { checkPrototypePollution } from './isConstructorOrProto'
 
-export function hasProperty<O extends object>(o: O, k: keyof any): k is keyof O {
+export function hasProperty<O extends object, K extends keyof any>(o: O, k: K): k is K & keyof O {
 	assert(typeof o === 'object')
 	checkPrototypePollution(o, k)
 	return k in o
