2b8c72c5a66b9121666c50d72871bcf84b472945
Adam Blaszkiewicz <adam.blaszkiewicz@voltiso.com>
2022-06-11 13:13:14 +0200

    add test

--- src/modules/object/UndefinedFromOptional.test.ts
+++ src/modules/object/UndefinedFromOptional.test.ts
@@ -5,12 +5,26 @@ import { Assert } from '../bdd'
 describe('UndefinedFromOptional', () => {
 	it('works', () => {
 		expect.assertions(0)
+
 		Assert<
 			IsIdentical<
 				UndefinedFromOptional<{ x?: number }>,
 				{ x?: number | undefined }
 			>
 		>()
+
 		Assert<IsIdentical<UndefinedFromOptional<{ x: number }>, { x: number }>>()
+	})
+
+	it('vscode jump to definition (manual test...)', () => {
+		expect.assertions(0)
+
+		type Obj = {
+			readonly a?: 0
+			b: 0
+		}
+
+		const obj = {} as unknown as UndefinedFromOptional<Obj>
+		void obj.a
 	})
 })
