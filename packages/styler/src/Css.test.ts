1919b36d98818699f5667a181fe56acdc7c4a2e9
Adam Blaszkiewicz <adam.blaszkiewicz@voltiso.com>
2022-08-04 19:15:23 +0200

    styler: allow React 18 peer, fix lint errors

--- packages/styler/src/Css.test.ts
+++ packages/styler/src/Css.test.ts
@@ -1,8 +1,14 @@
+// ⠀ⓥ 2022     🌩    🌩     ⠀   ⠀
+// ⠀         🌩 V͛o͛͛͛lt͛͛͛i͛͛͛͛so͛͛͛.com⠀  ⠀⠀⠀
+
 import { Assert } from '@voltiso/util'
+
 import type { Css } from '~/Css'
 
 describe('Css', () => {
 	it('simple', () => {
+		expect.assertions(0)
+
 		const a = {
 			height: '100vh',
 		} as const
@@ -11,6 +17,8 @@ describe('Css', () => {
 	})
 
 	it('allows arrays', () => {
+		expect.assertions(0)
+
 		const a = {
 			height: ['100vh', '-webkit-fill-available'],
 		} as const
