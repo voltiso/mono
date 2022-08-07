293fe02b3c6eb784b3386af8f436044cb45eb906
Adam Blaszkiewicz <adam.blaszkiewicz@voltiso.com>
2022-08-07 02:29:36 +0200

    styler: typings

--- packages/styler/src/StyledComponent/StyledComponent.ts
+++ packages/styler/src/StyledComponent/StyledComponent.ts
@@ -1,21 +1,17 @@
 // ⠀ⓥ 2022     🌩    🌩     ⠀   ⠀
 // ⠀         🌩 V͛o͛͛͛lt͛͛͛i͛͛͛͛so͛͛͛.com⠀  ⠀⠀⠀
 
-import type { OmitSignatures } from '@voltiso/util'
 import type { ForwardRefExoticComponent } from 'react'
 
-import type {
-	MergeProps as MergeProperties,
-	Props as Properties,
-} from '~/react-types'
-import type { IStylable, OuterProps as OuterProperties } from '~/Stylable'
+import type { MergeProps, Props } from '~/react-types'
+import type { IStylable, OuterProps } from '~/Stylable'
 import type { Styled_ } from '~/Styled'
 
+export type StyledComponentProps<P extends Props> = MergeProps<P, OuterProps>
+
 /** With Element already provided */
-interface StyledComponent<P extends Properties>
+interface StyledComponent<P extends Props>
 	extends Styled_<P, IStylable>,
-		ForwardRefExoticComponent<
-			MergeProperties<P, OmitSignatures<OuterProperties>>
-		> {}
+		ForwardRefExoticComponent<StyledComponentProps<P>> {}
 
 export type { StyledComponent as StyledComponent_ }
