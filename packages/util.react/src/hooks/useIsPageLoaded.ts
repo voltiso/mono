2f0281e8238dba051dddf3a6606cb886e818224b
Adam Blaszkiewicz <adam.blaszkiewicz@voltiso.com>
2022-08-08 07:28:17 +0200

    util.react useIsPageLoaded ssr fix

--- packages/util.react/src/hooks/useIsPageLoaded.ts
+++ packages/util.react/src/hooks/useIsPageLoaded.ts
@@ -4,16 +4,22 @@
 import { useEffect, useState } from 'react'
 
 export function useIsPageLoaded() {
-	if (typeof document === 'undefined') return true // ssr
+	if (typeof document === 'undefined') return false // ssr
 
 	// eslint-disable-next-line react-hooks/rules-of-hooks
 	const [isPageLoaded, setIsPageLoaded] = useState(
-		() => document.readyState === 'complete',
+		false, // consistent with server
+		// () => document.readyState === 'complete',
 	)
 
 	// eslint-disable-next-line react-hooks/rules-of-hooks
 	useEffect(() => {
 		if (isPageLoaded) return undefined
+
+		if (document.readyState === 'complete') {
+			setIsPageLoaded(true)
+			return undefined
+		}
 
 		const callback = () => {
 			setIsPageLoaded(true)
