038f56f21fbc09e4832ee4eab3c13403d5bfeb63
Adam Blaszkiewicz <adam.blaszkiewicz@voltiso.com>
2022-08-08 06:24:05 +0200

    util.react: add useIsPageLoaded

---
+++ packages/util.react/src/hooks/useIsPageLoaded.ts
@@ -0,0 +1,30 @@
+// ⠀ⓥ 2022     🌩    🌩     ⠀   ⠀
+// ⠀         🌩 V͛o͛͛͛lt͛͛͛i͛͛͛͛so͛͛͛.com⠀  ⠀⠀⠀
+
+import { useEffect, useState } from 'react'
+
+export function useIsPageLoaded() {
+	if (typeof document === 'undefined') return false // ssr
+
+	// eslint-disable-next-line react-hooks/rules-of-hooks
+	const [isPageLoaded, setIsPageLoaded] = useState(
+		() => document.readyState === 'complete',
+	)
+
+	// eslint-disable-next-line react-hooks/rules-of-hooks
+	useEffect(() => {
+		if (isPageLoaded) return undefined
+
+		const callback = () => {
+			setIsPageLoaded(true)
+		}
+
+		window.addEventListener('load', callback)
+
+		return () => {
+			window.removeEventListener('load', callback)
+		}
+	}, [isPageLoaded])
+
+	return isPageLoaded
+}
