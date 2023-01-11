07697313396b6a70db5436d1855909d34a06d0b7
Adam Blaszkiewicz <adam.blaszkiewicz@voltiso.com>
2023-01-11 03:24:55 +0100

    ...

--- packages/context/src/node/asyncHooks.ts
+++ packages/context/src/node/asyncHooks.ts
@@ -3,17 +3,25 @@
 
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
 export type AsyncHooksModule = typeof import('async_hooks')
+// export type AsyncHooksModule = typeof import('node:async_hooks')
 
 let _asyncHooks: AsyncHooksModule | undefined
 
 export function requireAsyncHooks(): AsyncHooksModule {
 	if (!_asyncHooks) {
 		try {
-			// eslint-disable-next-line n/global-require, unicorn/prefer-module
-			_asyncHooks = require('node:async_hooks') as AsyncHooksModule
+			// eslint-disable-next-line n/global-require, unicorn/prefer-module, unicorn/prefer-node-protocol
+			const asyncHooks = require('async_hooks') as
+				| Partial<AsyncHooksModule>
+				| undefined
+
+			// eslint-disable-next-line unicorn/error-message
+			if (!asyncHooks?.AsyncLocalStorage) throw new Error()
+
+			_asyncHooks = asyncHooks as never
 		} catch {
 			throw new Error(
-				`@voltiso/context: NodeContext requires 'node:async_hooks' module, which is not available in this NodeJS version. Please use 'ZoneContext' instead.`,
+				`@voltiso/context: NodeContext requires 'async_hooks' module, which is not available in this NodeJS version. Please use 'ZoneContext' instead.`,
 			)
 		}
 	}
